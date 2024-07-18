733be4ea07bd44de3a7d7d0c91a95b8845737ab9
Corrie Stoddard <corriestoddard@gmail.com>
2024-07-18 16:34:16 -0700

    Add dashboard

---
+++ dashboard/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+class ProtectedAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'This is a protected endpoint.'}
+        return Response(content)
