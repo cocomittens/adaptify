31727af0b6f18a07b857a47299fce1d303d55b29
Corrie Stoddard <corriestoddard@gmail.com>
2024-07-20 16:27:46 -0700

    Templates

--- adaptify/urls.py
+++ adaptify/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('accounts/', include('django.contrib.accounts.urls')),
 ]
