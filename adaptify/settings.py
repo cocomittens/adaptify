cf109194befb5281b1794427227b0cad7bf60221
Corrie Stoddard <corriestoddard@gmail.com>
2024-07-27 22:02:40 -0700

    Make it work

--- adaptify/settings.py
+++ adaptify/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'oauth2_provider',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +62,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 ROOT_URLCONF = 'adaptify.urls'
--- api/views.py
+++ api/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
 from .util.supabase_client import supabase
 from datetime import datetime
 import os
-import pickle
+from decouple import config
+from django.views.decorators.csrf import csrf_exempt
+import joblib
+import json
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'text_emotion_detection/models', 'emotion_classifier_pipe_lr.pkl')
+DEMO_USER_ID = config('DEMO_USER_ID')
 
 with open(MODEL_PATH, 'rb') as file:
-    model = pickle.load(file)
+    model = joblib.load(file)
 
-@login_required
+def predict_emotions(text):
+    results = model.predict([text])
+    return results[0]
+
+def get_prediction_proba(text):
+    results = model.predict_proba([text])
+    return results.max()
+
+@csrf_exempt
 def detect_emotion_text(request):
     if request.method == 'POST':
-        text = request.POST.get('text')
-        emotion = model.predict([text])[0]
-        confidence = model.predict_proba([text]).max()
+        data = json.loads(request.body.decode('utf-8'))
+        text = data.get('text')
 
-        supabase.from_('Emotions').insert([{ 'emotion': emotion, 'confidence': confidence, 'created_at': datetime.now().isoformat() }]).execute()
+        emotion = predict_emotions(text)
+        confidence = get_prediction_proba(text)
+
+        supabase.from_('Emotions').insert([{ 'user_id': DEMO_USER_ID, 'emotion': emotion, 'confidence': confidence, 'created_at': datetime.now().isoformat() }]).execute()
         return JsonResponse({'emotion': emotion, 'confidence': confidence})
+    else:
+        return HttpResponseBadRequest("Only POST method is allowed")
